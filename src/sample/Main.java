a7f152f7c4ed15ed8b9bcec8e1cd35b65484f0a0
samanatuoit <saman.yaghini@uoit.net>
2016-03-16 19:44:55 -0400

    Completed lab

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,10 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
@@ -36,7 +33,9 @@ public class Main extends Application {
         Label phoneNumLbl = new Label("Phone #");
         TextField phoneNumFld = new TextField();
         Label dobLbl = new Label("Date of Birth: ");
-        TextField dobFld = new TextField();
+        DatePicker datePicker = new DatePicker();
+        Label emailLbl = new Label("Email: ");
+        TextField emailFld = new TextField();
         Button registerBtn = new Button("Register");
 
         gridpane.add(userNameLbl, 0, 0);
@@ -45,11 +44,15 @@ public class Main extends Application {
         gridpane.add(passFld, 1, 1);
         gridpane.add(fullNameLbl, 0, 2);
         gridpane.add(fullNameFld, 1, 2);
-        gridpane.add(phoneNumLbl, 0, 3);
-        gridpane.add(phoneNumFld, 1, 3);
-        gridpane.add(dobLbl, 0, 4);
-        gridpane.add(dobFld, 1, 4);
-        gridpane.add(registerBtn, 0, 5);
+        gridpane.add(emailLbl, 0, 3);
+        gridpane.add(emailFld, 1, 3);
+        gridpane.add(phoneNumLbl, 0, 4);
+        gridpane.add(phoneNumFld, 1, 4);
+        gridpane.add(dobLbl, 0, 5);
+        gridpane.add(datePicker, 1, 5);
+        gridpane.add(registerBtn, 1, 6);
+
+        registerBtn.setOnAction(evt -> System.out.println(userFld.getText() + " " + fullNameFld.getText() + " " + emailFld.getText() + " " +  phoneNumFld.getText() + " " + datePicker.getValue()));
 
 
         //layout.setCenter(vbox);
